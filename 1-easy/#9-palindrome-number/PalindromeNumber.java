ea786dcca64f392eaa2c111f8ef8814164228c0e
Jaans Pravin Nadar <factomedia15@gmail.com>
2026-04-08 01:05:14 +0530

    LeetCode #9 Palindrome Number

---
+++ 1-easy/#9-palindrome-number/PalindromeNumber.java
@@ -0,0 +1,18 @@
+class PalindromeNumber {
+    public static boolean isPalindrome(int x){
+        if(x<0){
+            return false;
+        }
+        int temp = x;
+        int rev = 0 ;
+        while(x>0){
+            int digit =x%10;
+            rev = (rev * 10) + digit;
+            x = x/10;
+        }
+        return rev == temp;
+    }
+    public static void main (String [] args) {
+        System.out.println(isPalindrome(121));
+    }
+}
