fff82bd4fed81a57882eac49674cdefe54b2a71b
JaansPravinNadar <factomedia15@gmail.com>
2026-04-30 23:13:20 +0530

    Add LeetCode #412 FizzBuzz - Beats 100% Runtime ✅

---
+++ 1-easy/#58-length-of-last-word/LengthOfLastWord.java
@@ -0,0 +1,20 @@
+public class LengthOfLastWord {
+
+    public static int lengthOfLastWord(String s) {
+        s=s.trim();
+        int len=0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i)==' '){
+                break;
+            }
+            len++;
+        }
+        return len;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLastWord("Hello World")); // 5
+        System.out.println(lengthOfLastWord("   fly me   to   the moon  ")); // 4
+        System.out.println(lengthOfLastWord("luffy is still joyboy")); // 6
+    }
+}
