57c387d2f1e7d30a3a29597c89a4542f0eb3f447
Jaans Pravin Nadar <factomedia15@gmail.com>
2026-05-06 18:41:07 +0530

    Add LeetCode #136 Single Number - XOR approach Beats 99.97% 🏆

--- 1-easy/#136-single-number/SingleNumber.java
+++ 1-easy/#136-single-number/SingleNumber.java
@@ -1,11 +1,11 @@
-import java.util.HashMap;
+//import java.util.HashMap;
 
 public class SingleNumber {
 
     public static int singleNumber(int[] nums) {
 
         // Step 1: Count frequency of each element
-        HashMap <Integer,Integer> map=new HashMap<>();
+       /*  HashMap <Integer,Integer> map=new HashMap<>();
         for(int num:nums){
             map.put(num,map.getOrDefault(num,0)+1);
         }
@@ -14,7 +14,12 @@ public class SingleNumber {
                 return num;
             }
         }
-        return -1;
+        return -1;*/
+        int result=0;
+        for(int num:nums){
+            result=result^num;
+        }
+        return result;
     }
 
     public static void main(String[] args) {
